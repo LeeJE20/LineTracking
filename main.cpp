7dc52c25cd86e635263b3905d5f4deda4c17f28f
LeeJE20 <42895142+LeeJE20@users.noreply.github.com>
2020-11-16 08:45:28 +0900

    add and update functions
    add: findEdgeLines, addNewPoint, findMeanLine, drawLines
    
    update: makeROI is splited into hideNonROI and makeROI 
      - makeROI : return gray / fully equalized / partially(bottom part) equalized  image
      - hideNonROI : return image with non ROI is black. ROI is shape of rectangle / triangle / trapezoid (사다리꼴).

--- main.cpp
+++ main.cpp
@@ -5,28 +5,35 @@
 #include "opencv2/ximgproc.hpp"
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include<numeric>
 
 using namespace cv;
 using namespace std;
 
+
+#define PI 3.141592
+#define qSize 20
+
+
 class Histogram1D {
 private: int histSize[1]; // 히스토그램 빈도수 
 	   float hranges[2]; // 히스토그램 최소/최대 화소값 
-	   const float* ranges[1]; 
+	   const float* ranges[1];
 	   int channels[1]; // 1채널만 사용
 
 public: Histogram1D() { // 1차원 히스토그램을 위한 인자 준비 
-	histSize[0] = 256; 
-	hranges[0] = 0.0; 
-	hranges[1] = 255.0; 
-	ranges[0] = hranges; 
-	channels[0] = 0; }
+	histSize[0] = 256;
+	hranges[0] = 0.0;
+	hranges[1] = 255.0;
+	ranges[0] = hranges;
+	channels[0] = 0;
+}
 
-	  MatND getHistogram(const Mat &image){ 
-		  MatND hist; 
+	  MatND getHistogram(const Mat& image) {
+		  MatND hist;
 		  calcHist(&image, 1, channels, Mat(), hist, 1, histSize, ranges);
-					
-		  return hist; 
+
+		  return hist;
 	  }
 
 	  MatND equalize(const cv::Mat& image) {
@@ -37,84 +44,165 @@ public: Histogram1D() { // 1차원 히스토그램을 위한 인자 준비
 
 Mat IncreaseContrast(Mat img) {
 	Histogram1D h;
-
-	Mat result= h.equalize(img);
+	
+	//Mat result = img.clone();
+	Mat result = h.equalize(img);
 
 	return result;
 }
 
-Mat makeROI(Mat& img,int shape) {
-	Mat imgROI=img.clone();
+
+
+// 0: gray, 1: 전체 평활화, 2: 하단부 평활화
+Mat makeROI(Mat& img, int type) {
+	Mat imgROI = img.clone();
+
+	cvtColor(imgROI, imgROI, COLOR_BGR2GRAY);
+	
+	
+	
+	Mat underContrast = imgROI.clone();
+
+
+
+	if (type == 0)
+	{
+		GaussianBlur(imgROI, imgROI, Size(9, 9), 1.0);
+		return imgROI;
+	}
+	//Mat gray = imgROI.clone();
+
+
+
 
 	imgROI = IncreaseContrast(imgROI);
 	GaussianBlur(imgROI, imgROI, Size(9, 9), 1.0);
-	
-	switch (shape) {
-	case 0:
-		rectangle(imgROI, Point(0, 0), Point(img.cols, img.rows/2), Scalar(0, 0, 0),FILLED);
-		cvtColor(imgROI, imgROI,COLOR_BGR2GRAY);
 
-		return imgROI;
-	case 1: {
-		rectangle(imgROI, Point(0, 0), Point(img.cols, img.rows / 2), Scalar(0, 0, 0), FILLED);
-
-		Point rook_points[1][3];
-		rook_points[0][0] = Point(img.cols / 2, img.rows / 2);
-		rook_points[0][1] = Point(0, img.rows/2);
-		rook_points[0][2] = Point(0, img.rows);
-
-		const Point* ppt[1] = { rook_points[0] };
-		int npt[] = { 3 };
-		fillPoly(imgROI, ppt, npt, 1, Scalar(0, 0, 0));
-
-		Point rook_points2[1][3];
-		rook_points2[0][0] = Point(img.cols / 2, img.rows / 2);
-		rook_points2[0][1] = Point(img.cols, img.rows / 2);
-		rook_points2[0][2] = Point(img.cols, img.rows);
-
-		const Point* ppt2[1] = { rook_points2[0] };
-		int npt2[] = { 3 };
-		fillPoly(imgROI, ppt2, npt2, 1, Scalar(0, 0, 0));
-
-		cvtColor(imgROI, imgROI, COLOR_BGR2GRAY);
-
-		
-		
+	if (type == 1)
+	{
 		return imgROI;
 	}
-	case 2: {
-		rectangle(imgROI, Point(0, 0), Point(img.cols, img.rows / 2), Scalar(0, 0, 0), FILLED);
+
+
+	Rect bounds(0, 0, img.cols, img.rows);
+	Rect r(0, img.rows / 2, img.cols, img.rows / 2); // partly outside 
+	Mat roi = underContrast(r & bounds);
+	roi = IncreaseContrast(roi);
+	GaussianBlur(roi, roi, Size(9, 9), 1.0);
+	
+
+	// 하단부만 평활화한 영상 만들기
+	for (int j = img.rows / 2; j < img.rows; j++)
+	{
+		//j열의 주소(nc개만큼) 가져오기
+		uchar* data = underContrast.ptr<uchar>(j);
+		uchar* dataRoi = roi.ptr<uchar>(j - img.rows / 2);
+
+		for (int i = 0; i < img.cols; i++)
+		{
+			data[i] = dataRoi[i];
+		}
+
+	}
+	//imshow("tmp", underContrast);
+	if (type == 2)
+	{
+		return underContrast;
+	}
+
+
+
+}
+
+Mat hideNonROI(Mat& imgROI, int shape) {
+	//return imgROI;
+
+	switch (shape) {
+	case 0: {
+		rectangle(imgROI, Point(0, 0), Point(imgROI.cols, imgROI.rows / 2), Scalar(0, 0, 0), FILLED);
+		//imgROI = IncreaseContrast(imgROI);
+		//GaussianBlur(imgROI, imgROI, Size(9, 9), 1.0);
+		return imgROI;
+	}
+	case 1: {
+		rectangle(imgROI, Point(0, 0), Point(imgROI.cols, imgROI.rows / 2), Scalar(0, 0, 0), FILLED);
 
 		Point rook_points[1][3];
-		rook_points[0][0] = Point(img.cols / 4, img.rows / 2);
-		rook_points[0][1] = Point(0, img.rows/2);
-		rook_points[0][2] = Point(0, img.rows);
+		rook_points[0][0] = Point(imgROI.cols / 2, imgROI.rows / 2);
+		rook_points[0][1] = Point(0, imgROI.rows / 2);
+		rook_points[0][2] = Point(0, imgROI.rows);
 
 		const Point* ppt[1] = { rook_points[0] };
 		int npt[] = { 3 };
 		fillPoly(imgROI, ppt, npt, 1, Scalar(0, 0, 0));
 
 		Point rook_points2[1][3];
-		rook_points2[0][0] = Point(3*img.cols / 4, img.rows / 2);
-		rook_points2[0][1] = Point(img.cols, img.rows / 2);
-		rook_points2[0][2] = Point(img.cols, img.rows);
+		rook_points2[0][0] = Point(imgROI.cols / 2, imgROI.rows / 2);
+		rook_points2[0][1] = Point(imgROI.cols, imgROI.rows / 2);
+		rook_points2[0][2] = Point(imgROI.cols, imgROI.rows);
 
 		const Point* ppt2[1] = { rook_points2[0] };
 		int npt2[] = { 3 };
 		fillPoly(imgROI, ppt2, npt2, 1, Scalar(0, 0, 0));
 
-		cvtColor(imgROI, imgROI, COLOR_BGR2GRAY);
+		return imgROI;
+
+	}
+	
+	case 2: {
+		rectangle(imgROI, Point(0, 0), Point(imgROI.cols, imgROI.rows / 2), Scalar(0, 0, 0), FILLED);
+
+		Point rook_points[1][3];
+		rook_points[0][0] = Point(imgROI.cols / 4, imgROI.rows / 2);
+		rook_points[0][1] = Point(0, imgROI.rows / 2);
+		rook_points[0][2] = Point(0, imgROI.rows);
+
+		const Point* ppt[1] = { rook_points[0] };
+		int npt[] = { 3 };
+		fillPoly(imgROI, ppt, npt, 1, Scalar(0, 0, 0));
+
+		Point rook_points2[1][3];
+		rook_points2[0][0] = Point(3 * imgROI.cols / 4, imgROI.rows / 2);
+		rook_points2[0][1] = Point(imgROI.cols, imgROI.rows / 2);
+		rook_points2[0][2] = Point(imgROI.cols, imgROI.rows);
+
+		const Point* ppt2[1] = { rook_points2[0] };
+		int npt2[] = { 3 };
+		fillPoly(imgROI, ppt2, npt2, 1, Scalar(0, 0, 0));
 
 		return imgROI;
-		}	
+
+
+	}
+
+
+		return imgROI;
 	}
 
 }
 
+
+
+vector<Point> findEdgeLines(Mat original, Mat img, int lowThres = 100, int highThres = 300, const char* windowName = "fel img");
+
+vector<Point> find2Line(vector<Point> lines);
+void addNewPoint(vector<Point> lines);
+vector<Point> findMeanLine();
+Mat drawLines(Mat img, vector<Point> lines);
+
+Mat service(vector<Point> lines);
+
+queue<Point> p1;
+queue<Point> p2;
+
+
 int main(int argc, char** argv)
 {
-	VideoCapture cap(argv[1]);
 
+
+
+	//VideoCapture cap(argv[1]);
+	VideoCapture cap("clip3.mp4");
 	if (!cap.isOpened()) {
 		printf("Can't open the video");
 		return -1;
@@ -133,14 +221,344 @@ int main(int argc, char** argv)
 		}
 		imshow("camera img", img);
 		Mat imgROI = makeROI(img, 0);
+		imgROI = hideNonROI(imgROI, 0);
 		imshow("ROI img", imgROI);
+
+
 		Mat imgROI1 = makeROI(img, 1);
+		imgROI1 = hideNonROI(imgROI1, 0);
 		imshow("ROI1 img", imgROI1);
+
+
 		Mat imgROI2 = makeROI(img, 2);
+		imgROI2 = hideNonROI(imgROI2, 0);
 		imshow("ROI2 img", imgROI2);
+
+
+		
+		
+
+		vector<Point> lines;
+		lines = findEdgeLines(img, imgROI, 100, 300, "100~300");
+		lines = findEdgeLines(img, imgROI1, 100, 300, "imgROI1");
+		lines = findEdgeLines(img, imgROI2, 100, 300, "imgROI2");
+
+		//findEdgeLines(img, imgROI, 200, 300, "200~300");
+
+		//findEdgeLines(img, imgROI, 10, 300, "10~300");
+
+		//findEdgeLines(img, imgROI, 100, 200, "100~200");
+
+		//findEdgeLines(img, imgROI, 100, 400, "100~400");
+
+		//findEdgeLines(imgROI, 100, 300, "rec");
+		//findEdgeLines(imgROI1, 100, 300, "roi1");
+		//findEdgeLines(imgROI2, 100, 300, "roi2");
+
+
+		vector<Point> finalLines;
+		finalLines = find2Line(lines);
+
+		//if (finalLines.empty() && (!p1.empty()))
+		if (finalLines.empty()&& ( ! ( p1.empty()  )))
+		{
+			finalLines = findMeanLine();
+		}
+		else if (!finalLines.empty())
+		{
+			addNewPoint(finalLines);
+		}
+
+		
+		if (!finalLines.empty())
+		{
+			Mat lineImg;
+			lineImg = drawLines(img, finalLines);
+		}
+		
+
+		
 
 		if (waitKey(25) == 27)
 			break;
 	}
 	return 0;
+}
+
+
+
+vector<Point> findEdgeLines(Mat original, Mat img, int lowThres, int highThres, const char* windowName)
+{
+	
+	int kernel_size = 3;
+	
+	Mat contours = img.clone();
+	Mat dst1;
+	Canny(img, contours, lowThres, highThres, kernel_size);
+
+	dst1 = Scalar::all(0);
+	
+	img.copyTo(dst1, contours);
+	imshow(windowName, dst1);
+
+	// 선 감지 위한 허프 변환
+	//std::vector<cv::Vec2f> lines;
+	//cv::HoughLines(contours, lines, 1, CV_PI / 180, 40); 
+
+	// 선 그리기
+	//cv::Mat result(contours.rows, contours.cols, CV_8U, cv::Scalar(255));
+	//std::cout << "Lines detected: " << lines.size() << std::endl;
+
+	//// 선 벡터를 반복해 선 그리기
+
+
+
+	//std::vector<cv::Vec2f>::const_iterator it = lines.begin();
+	//while (it != lines.end()) {
+	//	float rho = (*it)[0]; // 첫 번째 요소는 rho 거리
+	//	float theta = (*it)[1]; // 두 번째 요소는 델타 각도
+	//	if (theta < PI / 4. || theta > 3. * PI / 4.) { // 수직 행
+	//		cv::Point pt1(rho / cos(theta), 0); // 첫 행에서 해당 선의 교차점
+	//		cv::Point pt2((rho - result.rows * sin(theta)) / cos(theta), result.rows);
+	//		// 마지막 행에서 해당 선의 교차점
+	//		cv::line(original, pt1, pt2, cv::Scalar(255), 1); // 하얀 선으로 그리기
+	//	}
+	//	//else { // 수평 행
+	//	//	cv::Point pt1(0, rho / sin(theta)); // 첫 번째 열에서 해당 선의 교차점
+	//	//	cv::Point pt2(result.cols, (rho - result.cols * cos(theta)) / sin(theta));
+	//	//	// 마지막 열에서 해당 선의 교차점
+	//	//	cv::line(original, pt1, pt2, cv::Scalar(255), 1); // 하얀 선으로 그리기
+	//	//}
+	//	std::cout << "line: (" << rho << "," << theta << ")\n";
+	//	++it;
+	//}
+
+
+
+	vector<Vec4i> lines;
+	cv::HoughLinesP(contours, lines, 1, CV_PI / 180, 30, 30, 5);
+
+
+
+	string b = "houph";
+	string houphWindow = windowName + b;
+
+	cv::namedWindow(houphWindow);
+	cv::imshow(houphWindow, original);
+
+
+
+	vector<Point> lp;
+
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		Vec4i l = lines[i];
+		//line(original, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2, 8);
+
+		Point p1 = Point(l[0], l[1]);
+		Point p2 = Point(l[2], l[3]);
+
+
+		double dy = l[3] - l[1];
+		double dx = l[2] - l[0];
+		double angle = atan(dy / dx) * (180.0 / CV_PI);
+		if (dx < 0.0) 
+		{
+			angle += 180.0;
+		}
+		else if (dy < 0.0) 
+		{
+			angle += 360.0;
+		}
+
+		if ((angle > 30 &&angle < 150) || (angle > 210 && angle <330))
+		{ // 수직 행
+			line(original, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2, 8);
+			//cout << "angle: " << angle << endl;
+
+			lp.push_back(p1);
+			lp.push_back(p2);
+		}
+		
+
+		cv::imshow(houphWindow, original);
+		//waitKey(0.1);
+	
+
+
+
+
+		//if (theta < PI / 4. || theta > 3. * PI / 4.) { // 수직 행
+
+
+		//line(img_lane, Point(l[0], l[1]), Point(l[2], l[3]), Scalar::all(255), 1, 8);
+	}
+
+
+
+	
+	return lp;
+
+}
+
+vector<Point> find2Line(vector<Point> lines)
+{
+	vector<Point> result;
+
+	if (!lines.empty())
+	{
+		Point lft1; // 왼쪽 1
+		Point lft2; // 왼쪽 2
+		Point rght1; // 오른쪽 1
+		Point rght2; // 오른쪽 2
+
+
+
+
+
+
+
+		// 결과 벡터 리턴
+		result.push_back(lft1);
+		result.push_back(lft2);
+		result.push_back(rght1);
+		result.push_back(rght2);
+	}
+
+	return result;
+}
+
+
+
+
+
+void addNewPoint(vector<Point> lines)
+{
+	// find2Line 결과로 찾은 라인이 있는 경우
+	if (!lines.empty())
+	{
+		for (int i = 0; i < 3; i+=2)
+		{
+			p1.push(lines[i]);
+			p2.push(lines[i+1]);
+
+			if (p1.size() > 20)
+			{
+				p1.pop();
+				p2.pop();
+			}
+
+		}
+	}
+}
+vector<Point> findMeanLine()
+{
+	queue<Point> tmp1(p1);
+	queue<Point> tmp2(p2);
+
+	vector<int> lft1x;
+	vector<int> lft1y;
+	vector<int> lft2x;
+	vector<int> lft2y;
+
+	vector<int> rght1x;
+	vector<int> rght1y;
+	vector<int> rght2x;
+	vector<int> rght2y;
+
+	vector<Point> result;
+
+	int i = 0;
+
+	// 큐에서 좌표 분리해내기
+	while (!tmp1.empty())
+	{
+		Point pt;
+		if (i % 2 == 0) // left
+		{
+			pt = tmp1.front();
+
+			lft1x.push_back(pt.x);
+			lft1y.push_back(pt.y);
+
+			pt = tmp2.front();
+			lft2x.push_back(pt.x);
+			lft2y.push_back(pt.y);
+			
+			tmp1.pop();
+			tmp2.pop();
+
+			i++;
+		}
+		else if (i % 2 == 1) // right
+		{
+			pt = tmp1.front();
+
+			rght1x.push_back(pt.x);
+			rght1y.push_back(pt.y);
+
+			pt = tmp2.front();
+			rght2x.push_back(pt.x);
+			rght2y.push_back(pt.y);
+
+			tmp1.pop();
+			tmp2.pop();
+
+			i++;
+		}
+	}
+
+	// 합
+	int lft1xS = accumulate(lft1x.begin(), lft1x.end(), 0);
+	int lft1yS = accumulate(lft1y.begin(), lft1y.end(), 0);
+	int lft2xS = accumulate(lft2x.begin(), lft2x.end(), 0);
+	int lft2yS = accumulate(lft2y.begin(), lft2y.end(), 0);
+
+	int rght1xS = accumulate(rght1x.begin(), rght1x.end(), 0);
+	int rght1yS = accumulate(rght1y.begin(), rght1y.end(), 0);
+	int rght2xS = accumulate(rght2x.begin(), rght2x.end(), 0);
+	int rght2yS = accumulate(rght2y.begin(), rght2y.end(), 0);
+
+	// 평균값으로 포인트 만들기
+	Point lft1 = ( Point( lft1xS / lft1x.size(), lft1yS / lft1y.size() ) );
+	Point lft2 = (Point(lft2xS / lft2x.size(), lft2yS / lft2y.size()));
+
+	Point rght1 = (Point(rght1xS / rght1x.size(), rght1yS / rght1y.size()));
+	Point rght2 = (Point(rght2xS / rght2x.size(), rght2yS / rght2y.size()));
+
+	// 결과 벡터 리턴
+	result.push_back(lft1);
+	result.push_back(lft2);
+	result.push_back(rght1);
+	result.push_back(rght2);
+
+	return result;
+}
+
+
+Mat drawLines(Mat img, vector<Point> lines)
+{
+	line(img, lines[0], lines[1], Scalar(0, 0, 255), 2, 8);
+	line(img, lines[2], lines[3], Scalar(0, 0, 255), 2, 8);
+
+	imshow("line img", img);
+	return img;
+}
+
+
+Mat service(vector<Point> lines)
+{
+	Mat img;
+	//int lft1xS = accumulate(lft1x.begin(), lft1x.end(), 0);
+	//int lft1yS = accumulate(lft1y.begin(), lft1y.end(), 0);
+	//int lft2xS = accumulate(lft2x.begin(), lft2x.end(), 0);
+	//int lft2yS = accumulate(lft2y.begin(), lft2y.end(), 0);
+
+	//int rght1xS = accumulate(rght1x.begin(), rght1x.end(), 0);
+	//int rght1yS = accumulate(rght1y.begin(), rght1y.end(), 0);
+	//int rght2xS = accumulate(rght2x.begin(), rght2x.end(), 0);
+	//int rght2yS = accumulate(rght2y.begin(), rght2y.end(), 0);
+
+
+	return img;
 }
